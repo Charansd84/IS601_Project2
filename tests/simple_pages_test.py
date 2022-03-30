57f75b23b5d0c21fd17d5104ea122747b7586556
sai <charandegala1998@gmail.com>
2022-03-30 02:14:10 -0400

    commit 21.

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,10 +5,10 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About Me</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Calculator</a>' in response.data
+    assert b'<a class="nav-link" href="/page1">Calculator and AAA testing</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Working Of Calculator</a>' in response.data
     assert b'<a class="nav-link" href="/page3">OOPS Concepts</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Article</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">SOLID principles</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -26,7 +26,7 @@ def test_request_page1(client):
     """This makes the index page"""
     response = client.get("/page1")
     assert response.status_code == 200
-    assert b"Calculator" in response.data
+    assert b"Calculator and AAA testing" in response.data
 
 def test_request_page2(client):
     """This makes the index page"""
@@ -44,7 +44,7 @@ def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
-    assert b"Article" in response.data
+    assert b"SOLID principles" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
